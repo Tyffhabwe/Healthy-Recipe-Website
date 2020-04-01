1624c02e19ac51a7ef6b25797af3d338aa7d984c
Tyff <tyffhabwe@gmail.com>
2020-04-02 11:52:14 +1300

    Database setup
    - I created the models.py page that allows me to store and create sql variables
    - I then migrated that data into my actual database for use.

--- virtualenv/mysite/food/models.py
+++ virtualenv/mysite/food/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
 # Create your models here.
+class Post(models.Model):
+	title = models.CharField(max_length=200)
+	image = models.CharField(max_length=500)
+	time_taken = models.IntegerField()
+	ingredients = models.TextField()
+	text = models.TextField()
+	published_date = models.DateTimeField(blank=True, null=True)
+
+	def publish(self):
+		self.published_date = timezone.now()
+		self.save()
+
+	def __str__(self):
+		return self.title
