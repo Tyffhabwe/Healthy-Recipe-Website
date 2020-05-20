b3b8b79500e3c6f38cc736c3d7653fb3d16f672e
Tyff <tyffhabwe@gmail.com>
2020-05-20 12:47:27 +1200

    Python 3.8
    - Fixed an issue that occured where the server would crash if the admin panel was accessed.
    - Updated python 3.7 to python 3.8, along with re-installing dependancies such as: requests, django, virtualenv, beautfulsoup4

--- virtualenv/mysite/food/views.py
+++ virtualenv/mysite/food/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, get_object_or_404, HttpResponse
 from bs4 import BeautifulSoup
 from .models import Post
 from django.utils import timezone
-from background_task import background
 import os
 import requests
 import csv
